c01a154e4109cfb0016d4d6bd5405a4367e6a811
Your Name <your-email@example.com>
2024-07-22 14:11:17 +0200

    done

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { fuels, manufacturers, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
-export default async function Home({ searchParams }) {
+export default async function Home({ searchParams }: { searchParams: any }) {
   const allCars = await fetchCars({
     manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
